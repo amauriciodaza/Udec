b829490fda39b554091992a57052525081b37d70
DanielMorales2000 <65742760+DanielMorales2000@users.noreply.github.com>
2020-11-08 02:03:30 -0500

    Cambio de material de la espada segun su energia

--- Assets/Programacion/Personaje/Einar/Assets/EnergySword.cs
+++ Assets/Programacion/Personaje/Einar/Assets/EnergySword.cs
@@ -2,17 +2,82 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class EnergySword : MonoBehaviour
 {
+    public GameObject[] lights;
+    public Material[] Materiales;
+
+    public GameObject SwordBlade;
+
+    float SwordLife;
+
+    bool UbEspada;
+
+    bool DownLife;
+
+    public bool superficie;
     // Start is called before the first frame update
     void Start()
     {
-        
+        SwordLife = 100;
+        DownLife = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        UbEspada = GetComponent<TranslationMovement>().armas;
+        if (UbEspada && DownLife && SwordLife > 0 && superficie)
+        {
+            Debug.Log("Bajo");
+            SwordLife = SwordLife - 0.25f;
+            DownLife = false;
+            StartCoroutine(DeathSword());
+        }
+        CurrentMaterial();
     }
+
+    void CurrentMaterial()
+    {
+        Renderer rend = SwordBlade.GetComponent<Renderer>();
+
+        if (SwordLife >= 66)
+        {
+            lights[0].SetActive(true);
+            lights[1].SetActive(false);
+            lights[2].SetActive(false);
+            rend.material = Materiales[0];
+        }
+        else if (SwordLife < 66 && SwordLife >= 33)
+        {
+            lights[0].SetActive(false);
+            lights[1].SetActive(true);
+            lights[2].SetActive(false);
+            rend.material = Materiales[1];
+        }
+        else if (SwordLife < 33 && SwordLife >= 5)
+        {
+            lights[0].SetActive(false);
+            lights[1].SetActive(false);
+            lights[2].SetActive(true);
+            rend.material = Materiales[2];
+        }
+        else
+        {
+            lights[0].SetActive(false);
+            lights[1].SetActive(false);
+            lights[2].SetActive(false);
+            rend.material = Materiales[3];
+        }
+    }
+    
+
+
+
+    IEnumerator DeathSword()
+    {
+        yield return new WaitForSeconds(0.5f);
+        DownLife = true;
+    }
+
 }
