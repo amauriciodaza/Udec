1920b03f7c12052c7f7aa61c78b1251c321e5882
amauriciodaza <amauriciodaza@ucundinamarca.edu.co>
2020-11-11 00:16:51 -0500

    Finalizando Subterraneo 60/100
    Anderson Daza

--- Assets/Programacion/Subterraneo/Puerta.cs
+++ Assets/Programacion/Subterraneo/Puerta.cs
@@ -5,15 +5,26 @@ using UnityEngine;
 public class Puerta : MonoBehaviour
 { 
     public GameObject Personaje;
+  
+
+    void Start()
+    {
+    
+    }
+
     // Start is called before the first frame update
     private void OnTriggerStay(Collider other)
     {
         if (other.name == Personaje.name)
         {
-            GetComponent<Animator>().SetBool("EstadoPuerta", true);
-            Debug.Log("Esta Abriendo");
-            GetComponent<AdministrarSonidos>().AbrirPuerta();
-                
+            if (Input.GetKey(KeyCode.F))
+            {
+                GetComponent<Animator>().SetBool("EstadoPuerta", true);
+                Debug.Log("Esta Abriendo");
+                GetComponent<AdministrarSonidos>().AbrirPuerta();
+            }                
         }
     }
+
+
 }
