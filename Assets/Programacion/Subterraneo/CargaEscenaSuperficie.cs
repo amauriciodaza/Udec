e44154bd3d5d2b8edb6de22b5f95f67661a6a8ae
amauriciodaza <amauriciodaza@ucundinamarca.edu.co>
2020-11-24 02:25:12 -0500

    Finalizacion Subterraneo
    Anderson Daza

---
+++ Assets/Programacion/Subterraneo/CargaEscenaSuperficie.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CargaEscenaSuperficie : MonoBehaviour
+{
+    public Text instruccion;   
+    // Start is called before the first frame update
+    void Start()
+    {
+        instruccion.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Einar")
+        {
+            instruccion.enabled = true;
+            instruccion.text = "Cargando...";
+            SceneManager.LoadScene("Superficie", LoadSceneMode.Single);
+            
+        }
+    }
+}
---
+++ Assets/Programacion/Subterraneo/RocasSub.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocasSub : MonoBehaviour
+{
+    GameObject Carcelero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Carcelero = GameObject.Find("CarceleroGeo");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Carcelero.GetComponent<EnemyLife>().life < 1) 
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+}
