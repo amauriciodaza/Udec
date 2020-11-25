80d1bf998bb658b35bdd8724981b2c4e4ed4d96d
amauriciodaza <amauriciodaza@ucundinamarca.edu.co>
2020-11-25 14:16:56 -0500

    Arreglo spawn en superficie
    Anderson Daza

--- Assets/Programacion/Subterraneo/CargaEscenaSuperficie.cs
+++ Assets/Programacion/Subterraneo/CargaEscenaSuperficie.cs
@@ -6,25 +6,32 @@ using UnityEngine.SceneManagement;
 
 public class CargaEscenaSuperficie : MonoBehaviour
 {
-    public Text instruccion;   
+    public Text instruccion;
+    private GameMaster gm;
+    GameObject Ajuste;
+    Vector3 AjusteV;
     // Start is called before the first frame update
     void Start()
     {
         instruccion.enabled = false;
+        gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+        Ajuste = GameObject.Find("Ajusteposicion");
+        AjusteV = new Vector3(0f, 2f, 0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(AjusteV+"Ajuste");
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.name == "Einar")
         {
+            gm.lastCheckPointPost = AjusteV;
             instruccion.enabled = true;
-            instruccion.text = "Cargando...";
+            instruccion.text = "CARGANDO...";
             SceneManager.LoadScene("Superficie", LoadSceneMode.Single);
             
         }
