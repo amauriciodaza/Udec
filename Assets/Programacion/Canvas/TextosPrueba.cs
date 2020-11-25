66c6945c4f4ef091660b3fd39e770498403ae0e0
jdanielsuarez98 <jdanielsuarez98@users.noreply.github.com>
2020-11-25 18:25:32 -0500

    Ortografia Texttos Subterraneo
    Daniel S

--- Assets/Programacion/Canvas/TextosPrueba.cs
+++ Assets/Programacion/Canvas/TextosPrueba.cs
@@ -107,21 +107,21 @@ public class TextosPrueba : MonoBehaviour
             texto = new string[]
             {
                 "Einar: ¡¿Qué es esto?!, de repente esa espada llameante salto a mi espalda, pensé que me quemaría, pero… Se siente una energía increíble en mi cuerpo...",
-                "Espada: 'LuxTerra, Luz de la tierra es mi nombre y tu eres mi destino'",
-                "Einar: ¿Que acabo de escuchar en mi mente?…El sable?… esta espada…",
-                "LuxTerra: Joven guerrero, no temas, el destino te ha elegido para portar los tesoros de los guardianes… Y la misión de recuperar el mundo que hace 20 años se perdió por los errores de tus antepasados… En este sable esta la esencia viva de la tierra misma, porta su energía, y su poder, como ya tu mismo lo pudiste sentir.",
-                "Einar: Osea que este es… El sable del que mi padre me contaba en sus historias. Pero si se supone eran solo historias.",
+                "Espada: 'LuxTerra, Luz de la tierra es mi nombre y tú eres mi destino'",
+                "Einar: ¿Que acabo de escuchar en mi mente? …El sable?… esta espada…",
+                "LuxTerra: Joven guerrero, no temas, el destino te ha elegido para portar los tesoros de los guardianes… Y la misión de recuperar el mundo que hace 20 años se perdió por los errores de tus antepasados… En este sable esta la esencia viva de la tierra misma, porta su energía, y su poder, como ya tú mismo lo pudiste sentir.",
+                "Einar: Ósea que este es… El sable del que mi padre me contaba en sus historias. Pero si se supone eran solo historias.",
                 "LuxTerra: A veces la realidad pasa a ser solo un cuento ya que no hay como o quien la atestigüe.",
-                "Einar: Es increíble… Mi padre me contaba que en el pasado la humanidad estaba llena de codicia y consumismo, destruían la naturaleza solo pensando en si mismos, y esto alimentaba a la esencia del caos, a Contaminación…",
-                "Einar: Sin embargo entre ellos habían personas que luchaban incansablemente para hacer entender a la humanidad el daño que estaban haciendo, sin embargo fueron ignorados y al final contaminacion cobro una fuerza descomunal y destruyo todo…",
-                "LuxTerra: Así es muchacho, Contaminación ha esperado por milenios para romper el equilibrio natural y alimentar el caos que porta, al final hace 20 años lo logro… Sin embargo no todo está perdido, supongo que también sabes del origen de los tesoros de los guardianes.",
-                "Einar: Por lo que me han y me has contado, solo se que existe una espada legendaria que nació del fuego y se perdió entre la tierra.",
-                "LuxTerra: Naci del fuego o más bien, fui creada del fuego de la tierra por la esencia de la naturaleza… ",
+                "Einar: Es increíble… Mi padre me contaba que en el pasado la humanidad estaba llena de codicia y consumismo, destruían la naturaleza solo pensando en sí mismos, y esto alimentaba a la esencia del caos, a Contaminación…",
+                "Einar: Sin embargo, entre ellos había personas que luchaban incansablemente para hacer entender a la humanidad el daño que estaban haciendo, sin embargo, fueron ignorados y al final contaminación cobro una fuerza descomunal y destruyo todo…",
+                "LuxTerra: Así es muchacho, Contaminación ha esperado por milenios para romper el equilibrio natural y alimentar el caos que porta, al final hace 20 años lo logro… Sin embargo, no todo está perdido, supongo que también sabes del origen de los tesoros de los guardianes.",
+                "Einar: Por lo que me han y me has contado, solo sé que existe una espada legendaria que nació del fuego y se perdió entre la tierra.",
+                "LuxTerra: Nací del fuego o más bien, fui creada del fuego de la tierra por la esencia de la naturaleza… ",
                 "LuxTerra: Hace más de 20 años cuando el caos estaba cerca, la naturaleza premio a dos guardianes por su luchar incansable y su amor inconmensurable por la vida en el mundo, los convirtió en seres trascendentales denominados Guardianes de las Energías, uno de ellos representa a la energía de la tierra, el otro a la energía del sol.",
-                "LuxTerra: Asi mismo les entrego a cada uno un tesoro, yo fui entregado a Feanor, Guardián de la Tierra; el tesoro del sol Sidus, fue entregado a Lena, la Guardiana del Sol.",
-                "LuxTerra: La naturaleza al entregarles los tesoros también les dio una profecía: Al pasar las décadas una nueva luz se levantara en la humanidad, su propio brillo le permitirá llegar a los tesoros que os he encomendado y la vida volverá nuevamente al mundo.",
+                "LuxTerra: Así mismo les entrego a cada uno un tesoro, yo fui entregado a Feanor, Guardián de la Tierra; el tesoro del sol Sidus, fue entregado a Lena, la Guardiana del Sol.",
+                "LuxTerra: La naturaleza al entregarles los tesoros también les dio una profecía: Al pasar las décadas una nueva luz se levantará en la humanidad, su propio brillo le permitirá llegar a los tesoros que os he encomendado y la vida volverá nuevamente al mundo.",
                 "LuxTerra: Einar, eres la esperanza de este mundo, debes ayudarme a encontrar a mi Guardián antes de que se entregado a Contaminación, lo necesitamos para poder enfrentar a ese monstruo. Siento que está cerca… en algún lugar de estos pasillos.",
-                "Einar: Esta bien, vamos!!"
+                "Einar: Esta bien, ¡vamos!
             };
             triggers[triggerAc].gameObject.GetComponent<Collider>().enabled = false;
             next = false;
@@ -131,7 +131,7 @@ public class TextosPrueba : MonoBehaviour
         {
             texto = new string[] 
             {
-                "Einar: ¿Trampas?, esto cada vez se pone mas raro y siniestro"
+                "Einar: ¿Trampas?, esto cada vez se pone más raro y siniestro"
             };
             triggers[triggerAc].gameObject.GetComponent<Collider>().enabled = false;
             next = false;
@@ -141,7 +141,7 @@ public class TextosPrueba : MonoBehaviour
         {
             texto = new string[]
             {
-                "LuxTerra: Estas son Runas, tomala, puede que la necesitemos"
+                "LuxTerra: Estas son Runas, tómala, puede que la necesitemos"
             };
             triggers[triggerAc].gameObject.GetComponent<Collider>().enabled = false;
             next = false;
@@ -151,7 +151,7 @@ public class TextosPrueba : MonoBehaviour
         {
             texto = new string[]
             {
-                "Einar: Parecen ser botones, supongo debo hacer algo aqui para poder pasar al otro lado"
+                "Einar: Parecen ser botones, supongo debo hacer algo aquí para poder pasar al otro lado"
             };
             triggers[triggerAc].gameObject.GetComponent<Collider>().enabled = false;
             next = false;
@@ -161,7 +161,7 @@ public class TextosPrueba : MonoBehaviour
         {
             texto = new string[]
             {
-                "Einar: Bien logre salir, ¿y ahora que sigue?... Parece ser un laberinto. A donde llegare..."
+                "Einar: Bien logre salir, ¿y ahora qué sigue?... Parece ser un laberinto. A donde llegare..."
             };
             triggers[triggerAc].gameObject.GetComponent<Collider>().enabled = false;
             next = false;
@@ -194,11 +194,12 @@ public class TextosPrueba : MonoBehaviour
             GetComponent<TranslationMovement>().DialogDetention();
             texto = new string[]
             {
-                "Einar: Pero que demonios es eso!!",
-                "LuxTerra: Es el destructor que atrapo a Feanor, ¡Mira al fondo, alla esta feanor!",
-                "Einar: ¡Mira al fondo, alla esta alguien atrapado!",
+                "Einar: ¡Pero qué demonios es eso!",
+                "LuxTerra: Es el destructor que atrapo a Feanor, ¡Mira al fondo, allá esta Feanor!",
+                "Einar: ¡Mira al fondo, allá esta alguien atrapado!",
                 "LuxTerra: Al fin, es Feanor, debes derrotar al destructor para liberarlo",
-                "Einar: Hoy me siento mas valiente de lo normal... Vamos!!!"
+                "Einar: ¡Hoy me siento más valiente de lo normal... Vamos!"
+
             };
             triggers[triggerAc].gameObject.GetComponent<Collider>().enabled = false;
             next = false;
@@ -210,8 +211,8 @@ public class TextosPrueba : MonoBehaviour
             texto = new string[]
             {
                 "Carcelero: No puede ser, quien es este humano",
-                "Einar: Con que estos son los servidores de contaminacion, ¡son unos demonios!",
-                "Einar: ¡Debo ir a liberar al Guardian!"
+                "Einar: Con que estos son los servidores de contaminación, ¡¡son unos demonios!!",
+                "Einar: ¡Debo ir a liberar al Guardián!"
             };
             triggers[triggerAc].gameObject.GetComponent<Collider>().enabled = false;
             next = false;
@@ -222,8 +223,8 @@ public class TextosPrueba : MonoBehaviour
             GetComponent<TranslationMovement>().DialogDetention();
             texto = new string[]
            {
-                "Einar: Siento que puedo abrirla con solo tocarla!!",
-                "LuxTerra: La puerta se mantiene cerrada por la fuerza de las energias no renovables, la pureza de las energias renovables que ahora portas, hace que puedas hacerlo"
+                "Einar: ¡Siento que puedo abrirla con solo tocarla!",
+                "LuxTerra: La puerta se mantiene cerrada por la fuerza de las energías no renovables, la pureza de las energías renovables que ahora portas, hace que puedas hacerlo"
            };
             triggers[triggerAc].gameObject.GetComponent<Collider>().enabled = false;
             next = false;
@@ -234,41 +235,41 @@ public class TextosPrueba : MonoBehaviour
             GetComponent<TranslationMovement>().DialogDetention();
             texto = new string[]
            {
-                "Feanor: Es increible, derrotaste a ese monstruo tu solo... Y me has salvado, te agradezco mucho",
-                "Feanor: Pude ver que portas a LuxTerra. Tu eres el joven guerrero que he esperado por tantos años.",
-                "Einar: ¿Tu eres el guardian del que me ha estado hablado LuxTerra?",
-                "Feanor: Asi es muchacho, yo soy Feanor, el guardian de la Energia Geotermica",
-                "Feanor: Te he escuchado a travez de LuxTerra, eres Einar, un descendiente de los humanos que habitan bajo la tierra en las cercanias",
+                "Feanor: Es increíble, derrotaste a ese monstruo tu solo... Y me has salvado, te agradezco mucho",
+                "Feanor: Pude ver que portas a LuxTerra. Tú eres el joven guerrero que he esperado por tantos años.",
+                "Einar: ¿Tu eres el guardián del que me ha estado hablado LuxTerra?",
+                "Feanor: Así es muchacho, yo soy Feanor, el guardián de la Energía Geotérmica",
+                "Feanor: Te he escuchado a través de LuxTerra, eres Einar, un descendiente de los humanos que habitan bajo la tierra en las cercanías",
                 "Feanor: Es un placer poder verte en persona al fin, y saber que vale la pena confiar en ti, tienes muchas agallas",
-                "Einar: Que!... Osea que la puedes oir a la espada sin necesidad de que este contigo?",
-                "Feanor: Esa espada esta basada en la energia geotermica, la energia de la tierra, y yo soy su guardian, nuestra esencia esta vinculada",
-                "Einar: Sorprendente!",
-                "Feanor: Ahora es tu espada, tu eres su portador, su maestro y ella tu guia, y su mision salvar a este mundo",
-                "Feanor: Escuchame:" ,
-                "Feanor: Desde hace 20 años la guardiana del sol y yo, hemos sido perseguidos por los destructores de Contaminacion",
-                "Feanor: Ella y yo como guardianes de las energias somo los protectores de la vida, por eso desean acabar con nosotros",
+                "Einar: ¡Que!... ¿Ósea que la puedes oír a la espada sin necesidad de que este contigo?",
+                "Feanor: Esa espada está basada en la energía geotérmica, la energía de la tierra, y yo soy su guardián, nuestra esencia está vinculada",
+                "Einar: ¡Sorprendente!",
+                "Feanor: Ahora es tu espada, tu eres su portador, su maestro y ella tu guía, y su misión salvar a este mundo",
+                "Feanor: Escúchame:”,
+                "Feanor: Desde hace 20 años la guardiana del sol y yo, hemos sido perseguidos por los destructores de Contaminación",
+                "Feanor: Ella y yo como guardianes de las energías somos los protectores de la vida, por eso desean acabar con nosotros",
                 "Feanor: Pero desafortunadamente nuestra fuerza no es suficiente para enfrentarle solos",
-                "Feanor: Sin embargo por ello estas tu, eres quien puede usar el poder completo de los tesoros, ya que tu corazon no esta lleno de ambicion y maldad, y tu sangre porta la conexion con la naturaleza y la vida",
+                "Feanor: Sin embargo, por ello estas tú, eres quien puede usar el poder completo de los tesoros, ya que tu corazón no está lleno de ambición y maldad, y tu sangre porta la conexión con la naturaleza y la vida",
                 "Feanor: Eres quien nace de la naturaleza para protegerla",
-                "Feanor: Al fin estas aqui, pero no puedo retenerte mas, debes seguir tu mision, yo tratare de apoyarte en tu mision alimentando la energia de tu espada a travez de los arboles de vida en la superficie",
-                "Einar: Superficie?, pero como ire alla, ademas, siempre me han dicho que no podemos volver alla",
-                "Einar: Que desde que todo aquello sucedio es un lugar lleno de muerte, el caos que la misma humanidad alimento con su ambicion e irresponsabilidad se convirtio en la esencia de la muerte misma",
-                "Einar: Cualquiera que vaya alla no llegara muy lejos",
-                "Feanor: Lo se, es de Contaminacion de quien hablas, la superficie esta poseida por ella, pero debes ir, el tiempo se agota, los recursos que quedan cada vez son menos",
-                "Feanor: La esencia de la naturaleza aun sigue presente, debes evitar que desaparezca por completo o no habra vuelta atras, la vida de tu mundo esta en tus manos",
-                "Einar: Pero... ¡¿Que deberia hacer yo?!",
-                "Feanor: La energia que en este momento emanas es muy poderosa, tu conexion con la espada tiene una sinergia perfecta, tu esencia y la de la espada se hacen una misma",
-                "Feanor: Esa energia que emana de ti es limpia y llena de vida, debes derrotar a los monstruos que habitan en la superficie, debes limpiar el mundo",
-                "Feanor: A medida que vayas derrotando a los servidores de Contaminacion, la naturaleza empezara a recobrar fuerza, esos seres consumen la vida, son la encarnacion de aquel deseo en los humanos que creo a Contaminacion",
-                "Einar: ¿Osea que los monstruos que atacan a mi pueblo son la forma de la maldad humana?, ese deseo de poder y placer que destruyo todo para su propia conveniencia",
-                "Feanor: Asi es, esos monstruos nacieron del corazon de la humanidad misma, el corazon de los humanos primo el placer sobre la vida, usaron energias que dañaron la naturaleza, y el resultado de ello se hizo una calamidad tomo vida",
+                "Feanor: Al fin estas aquí, pero no puedo retenerte más, debes seguir tu misión, yo tratare de apoyarte en tu misión alimentando la energía de tu espada a través de los árboles de vida en la superficie",
+                "Einar: ¿Superficie?, pero como iré allá, además, siempre me han dicho que no podemos volver allá",
+                "Einar: Que desde que todo aquello sucedió es un lugar lleno de muerte, el caos que la misma humanidad alimento con su ambición e irresponsabilidad se convirtió en la esencia de la muerte misma",
+                "Einar: Cualquiera que vaya allá no llegara muy lejos",
+                "Feanor: Lo sé, es de Contaminación de quien hablas, la superficie esta poseída por ella, pero debes ir, el tiempo se agota, los recursos que quedan cada vez son menos",
+                "Feanor: La esencia de la naturaleza aún sigue presente, debes evitar que desaparezca por completo o no habrá vuelta atrás, la vida de tu mundo está en tus manos",
+                "Einar: Pero... ¡¿Que debería hacer yo?!",
+                "Feanor: La energía que en este momento emanas es muy poderosa, tu conexión con la espada tiene una sinergia perfecta, tu esencia y la de la espada se hacen una misma",
+                "Feanor: Esa energía que emana de ti es limpia y llena de vida, debes derrotar a los monstruos que habitan en la superficie, debes limpiar el mundo",
+                "Feanor: A medida que vayas derrotando a los servidores de Contaminación, la naturaleza empezara a recobrar fuerza, esos seres consumen la vida, son la encarnación de aquel deseo en los humanos que creo a Contaminación",
+                "Einar: ¿Ósea que los monstruos que atacan a mi pueblo son la forma de la maldad humana?, ese deseo de poder y placer que destruyo todo para su propia conveniencia",
+                "Feanor: Así es, esos monstruos nacieron del corazón de la humanidad misma, el corazón de los humanos primo el placer sobre la vida, usaron energías que dañaron la naturaleza, y el resultado de ello se hizo una calamidad tomo vida",
                 "Einar: ¿Y entonces como lo hago?",
-                "Feanor: Sal de este templo, vuelve por donde viniste, cuando lo veas sabras cual es tu camino",
-                "Feanor: Cuando llegues a la superficie encuentra el amuleto de Lena, con el tendras la fuerza suficiente para derrotar a tus enemigos",
-                "Feanor: Ese amuleto porta la energia solar, es aquella energia que alimento la vida de la humanidad por milenios, una energia muy pura y poderosa, te llenara de fuerzas",
-                "Feanor: Luego los tesoros te guiaran en tu mision",
-                "Feanor: Ve Guardian!!",
-                "Einar: Lo hare, protegere este mundo!!"
+                "Feanor: Sal de este templo, vuelve por donde viniste, cuando lo veas sabrás cuál es tu camino",
+                "Feanor: Cuando llegues a la superficie encuentra el amuleto de Lena, con el tendrás la fuerza suficiente para derrotar a tus enemigos",
+                "Feanor: Ese amuleto porta la energía solar, es aquella energía que alimento la vida de la humanidad por milenios, una energía muy pura y poderosa, te llenara de fuerzas",
+                "Feanor: Luego los tesoros te guiaran en tu misión",
+                "Feanor: ¡Ve Guardián!",
+                "Einar: ¡Lo hare, protegeré este mundo!"
            };
             triggers[triggerAc].gameObject.GetComponent<Collider>().enabled = false;
             next = false;
@@ -278,7 +279,7 @@ public class TextosPrueba : MonoBehaviour
         {
             texto = new string[]
             {
-                "Einar: Habian unas piedras alli, creo es a lo que se referia Feanor, debo ir por aquellas escaleras, y llegare a la superficie"
+                "Einar: Había unas piedras allí, creo es a lo que se refería Feanor, debo ir por aquellas escaleras, y llegare a la superficie"
             };
             triggers[triggerAc].gameObject.GetComponent<Collider>().enabled = false;
             next = false;
